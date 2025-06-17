3e438f4c4164a17eff63db2edb8f5235db40dc33
elgbourisaad <elgbouri.saad@gmail.com>
2025-06-17 11:12:29 +0100

    added stuff

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import React, { useState, useEffect } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
@@ -42,4 +41,4 @@ function App() {
   );
 }
 
-export default App;
+export default App; 
